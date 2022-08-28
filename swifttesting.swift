89f60cc3805f4d81e56ed7eed355006583c82683
Ster-bent <104021908+Ster-bent@users.noreply.github.com>
2022-08-28 16:18:42 -0400

    update swift

--- swifttesting.swift
+++ swifttesting.swift
@@ -1,10 +1,11 @@
 ui = 10
-if ui == 10 {
-    print("Ten")
-}
-else if ui > 10 {
-    print("Greater than ten")
-}
-else if ui < 10 {
-    print("Less than ten")
+switch ui {
+case 0:
+    print("0")
+case 1,2,3,4,5:
+    print("half-way")
+case 6,7,8,9:
+    print("Near")
+default:
+    print("not in set")
 }
